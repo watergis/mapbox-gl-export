75d594c61a8233fe8d620c5a16598ac65ef5f7c2
watergis+githubci <watergis+githubci@users.noreply.github.com>
2021-03-22 10:48:50 +0000

    deploy to gh-pages

---
+++ index.js
@@ -0,0 +1,23 @@
+import mapboxgl from 'mapbox-gl';
+import { MapboxExportControl, Size, PageOrientation, Format, DPI} from '../lib/index';
+import '../css/styles.css';
+
+(()=>{
+    // mapboxgl.accessToken='your mapbox access token'
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // style: 'mapbox://styles/mapbox/streets-v11',
+        style:'https://narwassco.github.io/mapbox-stylefiles/unvt/style.json',
+        center: [35.87063, -1.08551],
+        zoom: 12,
+        hash:true,
+    });
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    map.addControl(new MapboxExportControl({
+        PageSize: Size.A3,
+        PageOrientation: PageOrientation.Portrait,
+        Format: Format.PNG,
+        DPI: DPI[96],
+        Crosshair: true,
+    }), 'top-right');
+})()
